cab35b4e804bbf9ba7e7a33b90b8a8fc966a20bf
Hakim Alabbasi <hakim.gx18@gmail.com>
2020-01-23 15:57:18 +0300

    files

---
+++ websockets-server.js
@@ -0,0 +1,19 @@
+var WebSocket = require('ws');
+var WebSocketServer= WebSocket.server;
+var port = 3001;
+var ws = new WebSocketServer({
+  port:port
+});
+
+console.log('websockets server started');
+ws.on('connection', function (socket) {
+console.log('client connection established');
+
+socket.on('message', function (data) {
+console.log('message received: ' + data);
+socket.send(data);
+});
+});
+
+
+    
